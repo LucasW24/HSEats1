5dd596f2ea58296aa5745b7eae2d1cc3a06a785b
LucasW24 <64482482+LucasW24@users.noreply.github.com>
2021-01-06 08:06:55 -0500

    dbsdifun

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useContext} from 'react'
-
+import {Button, Timeline}from 'antd'
 import AuthContext from '../auth/AuthContext.js'
 
 import Navbar from '../components/Navbar';
@@ -13,8 +13,16 @@ const Home = () => {
     return (
         <div className="App">
             <Navbar></Navbar>
+            
             <div id="body" style={{height:"120vh"}}>
                 Arnav is extremely awesome :) monke
+                <Button> Arnav </Button>
+                <Timeline mode = "right">
+                    <Timeline.Item>step1 2015-09-01</Timeline.Item>
+                    <Timeline.Item>step2 2015-09-01</Timeline.Item>
+                    <Timeline.Item>step3 2015-09-01</Timeline.Item>
+                    <Timeline.Item>step4 2015-09-01</Timeline.Item>
+                </Timeline>
             </div>
         </div>
     )
